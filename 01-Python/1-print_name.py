6809622704924893c98f5696ec5075569b884ee4
Devanshi2545 <ddevanshi60_be20@thapar.edu>
2023-11-02 00:53:59 +0530

    Here is you message

---
+++ 01-Python/1-print_name.py
@@ -0,0 +1,12 @@
+print("Devanshi")
+
+
+#second from user input
+#Making it dynamic
+ #we use 'input' word...It will take input from the user and show it as output
+input("what is your name?")
+#lets assign this to memory
+#This interprester is going to take this first line
+name=input("what is your name?")
+print(name)
+ 

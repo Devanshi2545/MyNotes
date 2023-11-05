7cff7bf6a3e8db74b3c9f4fe102b3050ce19f492
Devanshi2545 <ddevanshi60_be20@thapar.edu>
2023-11-05 22:36:22 +0530

    Here is you message

---
+++ 4-OPTIONAL.py
@@ -0,0 +1,10 @@
+#OPTIONAL
+complex
+#used for real and imaginary number
+bin
+#used to represent binary number of a number
+print(bin(5))
+#OUTPUT=0b101 here 0b represents that 101 is a binary form of 5
+#TO CONVERT BINARY INTO DECIMAL OR HEXA...
+print(int(0b101,10))
+#After , we will write number in which we want to conver binary into(dec,oct,hexa)
---
+++ 5-Variables.py
@@ -0,0 +1,19 @@
+#VARIABLES-store information used in programs.we can hol values
+q=190 #here q is variable
+print(q)
+#snake_case means no space
+#ex
+user_iq=20
+#starts with lowercase or underscore
+#letters,numbers,underscores
+us4r=70
+#case sensitive
+#dont overwrite keywords
+#ex- we cannot use 'print' as a variable
+#--------------------------------------------------------------------------------
+#TYPES OF VARIABLES
+#1.constants-variables that cant be changed
+PI=3.14
+#2.__(DUNDERS)->starting with two underscores
+a,b,c=1,2,3
+#way of assigning values to variables short handedly(shortcut)

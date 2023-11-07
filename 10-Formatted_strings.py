13cd35ce553d21a377df30da7003e0046b0b8c45
Devanshi2545 <ddevanshi60_be20@thapar.edu>
2023-11-07 17:21:35 +0530

    Here is you message

--- 10-Formatted_strings.py
+++ 10-Formatted_strings.py
@@ -2,7 +2,7 @@
 #example you have to make an amazon user page
 name = 'Devanshi'
 age = 21
-#print('hi ' + name +'. you are '+str(age) +' years old')
+print('hi ' + name +'. you are '+str(age) +' years old')
 print(f'hi {name}. You are {age} years old.')
 #The f before the string indicates that it's an f-string, allowing you to include variables and expressions inside curly braces.
 #to make it dynamic we have to use 'f' word in starting of line and variables in curly brackets .
@@ -17,3 +17,4 @@ print('hi {}.You are {} years old'.format(name,age))
 print('hi {new_name}. You are {age} years old'.format(new_name='Sally',age=100))
 #we know counting starts from 0 ,1so if we give numbering in the brackets it would pick according to number sequence
 print('hi {0}.You are {1} years old'.format(name,age))
+print('hi {1}.You are {0} years old'.format(name,age))
